4c0f3db6db13516bb177f9abe7bdd8be791e092e
iamabdoulaziz <abdoulazizc867@gmail.com>
2026-06-19 12:19:07 +0000

    First commit

---
+++ src/main/java/groupe3/example/santekunafoniapp/SanteKunafoniAppApplication.java
@@ -0,0 +1,13 @@
+package groupe3.example.santekunafoniapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanteKunafoniAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanteKunafoniAppApplication.class, args);
+    }
+
+}
