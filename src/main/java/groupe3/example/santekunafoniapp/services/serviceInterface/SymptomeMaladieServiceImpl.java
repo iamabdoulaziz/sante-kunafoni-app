85779c08202d719bf236284c5ca59fa643b55241
Mohamed Maiga <medmo7374@gmail.com>
2026-06-22 12:14:48 +0000

    ajout de SymptomMaladie....

--- src/main/java/groupe3/example/santekunafoniapp/services/serviceInterface/SymptomeMaladieServiceImpl.java
+++ src/main/java/groupe3/example/santekunafoniapp/services/serviceInterface/SymptomeMaladieServiceImpl.java
@@ -1,10 +1,10 @@
-package groupe3.example.santekunafoniapp.services;
+package groupe3.example.santekunafoniapp.services.serviceInterface;
 
 import groupe3.example.santekunafoniapp.DTO.SymptomeMaladieDTO;
 
 import java.util.List;
 
-public interface SymptomeMaladieService {
+public interface SymptomeMaladieServiceImpl{
     SymptomeMaladieDTO ajouter(SymptomeMaladieDTO dto);
     List<SymptomeMaladieDTO> getAll();
     List<SymptomeMaladieDTO> getByMaladie(Integer idMaladie);
